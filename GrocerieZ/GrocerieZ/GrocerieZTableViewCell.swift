006c0c4b1743adf8006e102e5f3dce25b8fc37d0
kage <katharina.gerken@zuehlke.com>
2014-11-23 17:49:40 +0100

    Initial app commit

---
+++ GrocerieZ/GrocerieZ/GrocerieZTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  GrocerieZTableViewCell.swift
+//  GrocerieZ
+//
+//  Created by kage on 23.11.14.
+//  Copyright (c) 2014 Zühlke Engineering. All rights reserved.
+//
+
+import UIKit
+
+class GrocerieZTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ GrocerieZ/GrocerieZ/GrocerieZTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  GrocerieZTableViewController.swift
+//  GrocerieZ
+//
+//  Created by kage on 23.11.14.
+//  Copyright (c) 2014 Zühlke Engineering. All rights reserved.
+//
+
+import UIKit
+
+class GrocerieZTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    var items: [String] = ["Eggs", "Milk"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> GrocerieZTableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("groceriezCell", forIndexPath: indexPath) as GrocerieZTableViewCell
+        cell.textField.text = items[indexPath.row]
+        cell.textField.tag = indexPath.row
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            items.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+
+    // MARK: UITextField delegate
+    
+    func textFieldDidEndEditing(textField: UITextField!) {
+        items[textField.tag] = textField.text
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // MARK: Custom methods
+    
+    @IBAction func addItem(sender: AnyObject) {
+        items.append("")
+        tableView.reloadData()
+        let cell = self.tableView.cellForRowAtIndexPath(NSIndexPath(forRow: items.count - 1, inSection: 0)) as GrocerieZTableViewCell
+        cell.textField.becomeFirstResponder()
+    }
+}
