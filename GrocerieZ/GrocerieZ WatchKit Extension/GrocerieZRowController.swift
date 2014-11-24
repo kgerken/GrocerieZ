002f8fe404739c6561c800aab2f42281f496213a
kage <katharina.gerken@zuehlke.com>
2014-11-24 17:55:32 +0100

    Added table to display items on watch

---
+++ GrocerieZ/GrocerieZ WatchKit Extension/GrocerieZRowController.swift
@@ -0,0 +1,15 @@
+//
+//  GrocerieZRowController.swift
+//  GrocerieZ
+//
+//  Created by kage on 24.11.14.
+//  Copyright (c) 2014 Zühlke Engineering. All rights reserved.
+//
+
+import WatchKit
+
+class GrocerieZRowController: NSObject {
+    
+    @IBOutlet weak var cellLabel: WKInterfaceLabel!
+   
+}
--- GrocerieZ/GrocerieZ WatchKit Extension/InterfaceController.swift
+++ GrocerieZ/GrocerieZ WatchKit Extension/InterfaceController.swift
@@ -12,13 +12,14 @@ import GrocerieZKit
 
 class InterfaceController: WKInterfaceController {
 
-    @IBOutlet weak var testLabel: WKInterfaceLabel!
+    @IBOutlet weak var itemsTable: WKInterfaceTable!
     
     var items:GKItems = GKItems()
     
     override init(context: AnyObject?) {
         // Initialize variables here.
         super.init(context: context)
+        setTitle("GocerieZ")
         //NSNotificationCenter.defaultCenter().addObserver(self, selector: "appItemsChanged:", name: NSUserDefaultsDidChangeNotification, object: nil)
         //userDefaults.addObserver(self, forKeyPath: "items", options: NSKeyValueObservingOptions.New, context: nil)
         // Configure interface objects here.
@@ -38,13 +39,27 @@ class InterfaceController: WKInterfaceController {
         super.willActivate()
         NSLog("%@ will activate", self)
         
-        testLabel.setText(items.description)
+        loadTableData()
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         NSLog("%@ did deactivate", self)
         super.didDeactivate()
+    }
+    
+    private func loadTableData() {
+        itemsTable.setNumberOfRows(items.count, withRowType: "groceriezWatchCell")
+        for i in 0...items.count - 1 {
+            let row = itemsTable.rowControllerAtIndex(i) as GrocerieZRowController
+            row.cellLabel.setText(items[i])
+        }
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        items.removeAtIndex(rowIndex)
+        items.save()
+        loadTableData()
     }
 
 }
