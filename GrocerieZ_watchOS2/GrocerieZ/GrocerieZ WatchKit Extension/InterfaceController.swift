33b1ad1b84cd35175eabaa235f9253c6794fcaa6
kage <katharina.gerken@zuehlke.com>
2015-12-02 09:23:51 +0100

    Added initial watchOS 2 project

---
+++ GrocerieZ_watchOS2/GrocerieZ/GrocerieZ WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  GrocerieZ WatchKit Extension
+//
+//  Created by kage on 02.12.15.
+//  Copyright © 2015 Zühlke Engineering. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ GrocerieZ_watchOS2/GrocerieZ/GrocerieZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrocerieZ
+//
+//  Created by kage on 02.12.15.
+//  Copyright © 2015 Zühlke Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
