2c6367335e1e7f4b9f4670d931088818db2e8d5b
kage <katharina.gerken@zuehlke.com>
2015-12-03 15:18:12 +0100

    Added glance showing current number of items on the list

--- GrocerieZ_watchOS2/GrocerieZ/GrocerieZ WatchKit Extension/GlanceController.swift
+++ GrocerieZ_watchOS2/GrocerieZ/GrocerieZ WatchKit Extension/GlanceController.swift
@@ -12,20 +12,27 @@ import Foundation
 
 class GlanceController: WKInterfaceController {
 
+    @IBOutlet var itemsCountLabel: WKInterfaceLabel!
+    var timer:NSTimer!
+    let extensionDelegate = WKExtension.sharedExtension().delegate as! ExtensionDelegate
+    
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
     }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        refresh()
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "refresh", userInfo: nil, repeats: true)
     }
 
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        timer.invalidate()
+    }
+    
+    private func refresh() {
+        itemsCountLabel.setText(String(extensionDelegate.items.count))
     }
 
 }
